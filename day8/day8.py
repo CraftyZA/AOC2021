f6a9094d67e534b1282ce3742cbaddae14ef6b47
Ben Fourie <bfourie@gmail.com>
2021-12-09 09:21:24 +0200

    Day 8 Part 2

--- day8/day8.py
+++ day8/day8.py
@@ -1,6 +1,9 @@
+from itertools import permutations
+
+
 def get_payload():
     with open('day8.txt') as file:
-        return file.read().splitlines()
+        return file.readlines()
 
 
 def q1():
@@ -14,7 +17,33 @@ def q1():
 
 
 def q2():
-    print()
+    wiring = {
+        "abcefg": 0,
+        "cf": 1,
+        "acdeg": 2,
+        "acdfg": 3,
+        "bcdf": 4,
+        "abdfg": 5,
+        "abdefg": 6,
+        "acf": 7,
+        "abcdefg": 8,
+        "abcdfg": 9,
+    }
+    output = 0
+    data = get_payload()
+    for line in data:
+        a, b = line.split(" | ")
+        a = a.split()
+        b = b.split()
+        for permutation in permutations("abcdefg"):
+            to = str.maketrans("abcdefg", "".join(permutation))
+            _a = ["".join(sorted(code.translate(to))) for code in a]
+            _b = ["".join(sorted(code.translate(to))) for code in b]
+            if all(code in wiring for code in _a):
+                output += int("".join(str(wiring[code]) for code in _b))
+                break
+    print(output)
 
 
 q1()
+q2()
